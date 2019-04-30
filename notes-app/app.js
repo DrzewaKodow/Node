1033f37741004709898efd2067469874e5d82794
Piotr Russ <mail@pruss.it>
2019-04-30 23:36:29 +0200

    added yargs commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,48 @@
 const getNotes = require('./notes.js')
+const chalk = require('chalk')
+const yargs = require('yargs')
 
-console.log(getNotes())
+//Customize Yargs version
+yargs.version('1.0.1')
+
+//add command
+yargs.command({
+	command: 'add',
+	describe: 'Adding a new note',
+	builder: {
+		title: {
+			describe: 'Note title',
+			require: true,
+			type: String
+		},
+		body: {
+			describe: 'Note body',
+			require: true,
+			type: String
+		}
+	},
+	handler: (argv) => console.log('Title: ' + argv.title + ', Body: ' + argv.body)
+})
+
+//remove command
+yargs.command({
+	command: 'remove',
+	describe: 'Removing a note',
+	handler: () => console.log('Removing a note')
+})
+
+//list command
+yargs.command({
+	command: 'list',
+	describe: 'Listing all notes',
+	handler: () => console.log('Listing all notes')
+})
+
+//read command
+yargs.command({
+	command: 'read',
+	describe: 'Reading a note',
+	handler: () => console.log('Reading a note')
+})
+
+yargs.parse();

03acf50256acf151862a720c15cc097ebf8bf80e
Piotr Russ <mail@pruss.it>
2019-05-17 23:57:30 +0200

    web server to weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+
 const app = express()
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -14,10 +24,41 @@ app.get('', (req, res) => {
 	})
 })
 
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About page',
+		name: 'Pruss'
+	})
+})
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help page',
+		text: 'Some helpful text',
+		name: 'Pruss'
+	})
+})
+
 app.get('/weather', (req, res) => {
 	res.send({
 		location: 'xxxx',
 		forecast: 'aaaa'
+	})
+})
+
+app.get('/help/*', (req, res) => {
+	res.render('help', {
+		title: 'Help page not found',
+		text: 'There is no help page at this address',
+		name: 'Pruss'
+	})
+})
+
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: 'Page not found',
+		text: 'Could not find page at given address',
+		name: 'Pruss'
 	})
 })
 
